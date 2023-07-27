c35bcb8f64a4d512fd1efdc65ecf413c03093968
Hazem Mohammad <hazem743@hotmail.com>
2023-07-27 22:23:24 +0200

    end of l nr.11

--- c01/starter/script.js
+++ c01/starter/script.js
@@ -1,2 +1,10 @@
 let js = "fun";
 // if(js==='fun')alert('js is really fun')
+
+let firstName = "Hazem";
+console.log(firstName);
+
+let WILL = 'always great';
+console.log(typeof(WILL));
+
+let PI = 3.1415
